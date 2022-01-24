60ac206b8cc21b043d8e288c50fa9f335bf1ce91
sofia-sofista <82372136+sofia-sofista@users.noreply.github.com>
2022-01-24 04:11:08 -0600

    Add files via upload

---
+++ js/figura.js
@@ -0,0 +1,9 @@
+<script>
+$(document).ready(function(){
+    $('img.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
+</script>
